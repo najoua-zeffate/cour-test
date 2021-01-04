ad51b303e3a88175c10baee98bd1fdfaa07941ba
unknown <najoua.zeff@gmail.com>
2021-01-04 11:52:22 +0100

    Mod5_S

--- site1/mod5-solution/js/script.js
+++ site1/mod5-solution/js/script.js
@@ -42,7 +42,7 @@ var insertHtml = function (selector, html) {
 // Show loading icon inside element identified by 'selector'.
 var showLoading = function (selector) {
   var html = "<div class='text-center'>";
-  html += "<img src='image/ajax-loader.gif'></div>";
+  html += "<img src='images/ajax-loader.gif'></div>";
   insertHtml(selector, html);
 };
 
